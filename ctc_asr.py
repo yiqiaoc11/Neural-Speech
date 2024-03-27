0dd20b5373fa0188e5f0cc98526c2b6e30e84db4
yiqiaoc11 <30539007+yiqiaoc11@users.noreply.github.com>
2024-03-26 18:30:35 -0700

    Update ctc_asr.py

--- ctc_asr.py
+++ ctc_asr.py
@@ -2,9 +2,6 @@ import torchaudio
 train_dataset = torchaudio.datasets.LIBRISPEECH("./", url="train-clean-100", download=True)
 test_dataset = torchaudio.datasets.LIBRISPEECH("./", url="test-clean", download=True)
 
-torchaudio.transforms.FrequencyMasking()
-torchaudio.transforms.TimeMasking()
-
 char_map_str = """
  ' 0
  <SPACE> 1
